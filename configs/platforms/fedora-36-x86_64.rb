cf732ce8ce05d9bde0ed71d5809564cb6355232d
Shubham Shinde <shubham.shinde@perforce.com>
2024-04-30 18:28:37 +0530

    Removed binutils

--- configs/platforms/fedora-36-x86_64.rb
+++ configs/platforms/fedora-36-x86_64.rb
@@ -1,6 +1,3 @@
 platform 'fedora-36-x86_64' do |plat|
   plat.inherit_from_default
-
-  packages = %w[binutils]
-  plat.provision_with("/usr/bin/dnf install -y --best --allowerasing #{packages.join(' ')}")
 end
